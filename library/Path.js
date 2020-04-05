3f8b9fbd04584bdf12e1544e6598e2217e8ce044
xaota <1172619+xaota@users.noreply.github.com>
2020-04-05 16:32:53 +0500

    правки

--- library/Path.js
+++ library/Path.js
@@ -5,7 +5,7 @@ import Context from './Context.js';
 /** {Path} Рисование путей @export @class */
   export default class Path extends Context {
   /** Создание пути рисования
-    * @param {Path2D|SVGPath} path? another path object
+    * @param {Path2D} path? another path object
     */
     constructor(path) {
       path = new Path2D(path);
